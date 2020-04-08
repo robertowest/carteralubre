7ce438690fa58d8f5e7994d4f97cb4489631d15a
robertowest <roberto.west@gmail.com>
2020-04-08 17:35:53 -0300

    modificaciones en Heroku

--- heroku.sh
+++ heroku.sh
@@ -2,4 +2,8 @@
 git add -A .
 git commit -m "modificaciones en Heroku"
 git push heroku master
-heroku run python manage.py migrate --settings=config.settings-heroku
+
+# heroku run python manage.py migrate --settings=config.settings-heroku
+# heroku run python manage.py createsuperuser --settings=config.settings-heroku
+# heroku run python manage.py makemigrations --settings=config.settings-heroku
+# heroku run python manage.py migrate --settings=config.settings-heroku
